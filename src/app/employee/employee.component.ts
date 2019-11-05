b977b46c04fa95c07c8680160b033cf51c94de99
Andrei Vasiliu <andrei@anvaplus.com>
2019-11-05 12:56:20 +0100

    event binding

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,21 +1,38 @@
-import {Component} from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
-    selector : 'my-employee',
-    templateUrl : './employee.component.html',
+    selector: 'my-employee',
+    templateUrl: './employee.component.html',
     styleUrls: ['./employee.component.css']
 })
 
 
-export class EmployeeComponent{
-    firstName : string = 'Valentino';
-    lastName : string = 'Rossi';
-    gender : string= 'Male';
+export class EmployeeComponent {
+    firstName: string = 'Valentino';
+    lastName: string = 'Rossi';
+    gender: string = 'Male';
     age = 40;
 
-    isDisabled : boolean = false;
+    isDisabled: boolean = false;
 
-    getFullName(): string{
-        return this.firstName + ' ' + this.lastName; 
+    getFullName(): string {
+        return this.firstName + ' ' + this.lastName;
     }
+
+    onClickForward(): void {
+
+        if(this.age == 90){
+            return;
+        }
+        this.age =+ this.age + 1
+    }
+
+    onClickBackward(): void {
+
+        if(this.age == 10){
+            return;
+        }
+        this.age =+ this.age - 1
+    }
+
 }
